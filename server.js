05da681ada5d1994c14c3e3ae4f69f029e9f11b5
petitechine <petitechine01@gmail.com>
2024-10-14 02:04:50 +0000

    mise a jour teste

--- server.js
+++ server.js
@@ -1,7 +1,50 @@
-const express = require('express');
-const app = express();
+// import { createServer } from 'http'
+// import { parse } from 'url'
+// import next from 'next'
+const {createServer}=require("http")
+const {parse}=require("url")
+const next = require("next")
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur le port ${PORT}`);
-});
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
+
+
+
+
+
+// import { createServer } from 'http'
+// import { parse } from 'url'
+// import next from 'next'
+
+// const port = parseInt(process.env.PORT || '3000', 10)
+// const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev })
+// const handle = app.getRequestHandler()
+
+// app.prepare().then(() => {
+//   createServer((req, res) => {
+//     const parsedUrl = parse(req.url, true)
+//     handle(req, res, parsedUrl)
+//   }).listen(port)
+
+//   console.log(
+//     `> Server listening at http://localhost:${port} as ${
+//       dev ? 'development' : process.env.NODE_ENV
+//     }`
+//   )
+// })
